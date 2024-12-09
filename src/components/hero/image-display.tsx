c44004770d25a1718321a848c7c4eb6162a7aa4e
Khalid Ansari <khaliljpc@gmail.com>
2024-12-09 21:35:53 +0530

    fix: static image import for hero

--- src/components/hero/image-display.tsx
+++ src/components/hero/image-display.tsx
@@ -1,32 +1,28 @@
 import Image from 'next/image';
+import table from '../../../public/table.png';
+import tableDark from '../../../public/table-dark.png';
+import tableLight from '../../../public/table-light.png';
 
 const ImageDisplay = () => {
   return (
     <div id="1st" className="bg-cardBg w-full rounded-md p-3 flex">
       <div className="space-y-2">
         <div className="rounded-sm bg-white flex items-center justify-center w-14 h-14">
-          <Image src="/table-dark.png" alt="Table img" width={44} height={44} />
+          <Image src={tableDark} alt="Table img" width={44} height={44} />
         </div>
         {Array.from({ length: 3 }).map((_, i) => (
           <div key={i} className="rounded-sm bg-white w-fit">
-            <Image
-              src="/table-light.png"
-              alt="Table img"
-              width={55}
-              height={55}
-            />
+            <Image src={tableLight} alt="Table img" width={55} height={55} />
           </div>
         ))}
       </div>
       <div className="m-12">
         <Image
           className="mix-blend-multiply"
-          src="/table.png"
+          src={table}
           alt="Table img"
           width={552}
           height={552}
-          placeholder="blur"
-          blurDataURL="/blur-table.webp"
         />
       </div>
     </div>
